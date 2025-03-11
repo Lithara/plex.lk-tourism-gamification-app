4d4a11a08911c2ae2344a1e39d69c3fd438e4637
Lithara <litharaperera2002@gmail.com>
2025-03-11 21:40:45 +0530

    landing page changed

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,46 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+import { Flag } from "lucide-react";
+import Link from "next/link";
+
 export default function Home() {
-  return <div className="h-[2000px]">hello</div>;
+  return (
+    <main className="min-h-screen  mx-[16px] sm:mx-[64px] md:mx-[120px] ">
+      {/* Hero Section */}
+      <section className="container px-4 pt-8 md:pt-12 lg:pt-16 ">
+        <div className="grid gap-8 lg:grid-cols-2 lg:gap-12 items-center">
+          <div className="space-y-4">
+            <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold tracking-tight">
+              Explore,{" "}
+              <Flag className="inline-block w-8 h-8 text-red-500 -mt-2" />
+              <br />
+              Play <span className="text-red-500">&</span> Share.
+            </h1>
+            <p className="text-lg text-muted-foreground max-w-[600px]">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et
+              massa mi. Aliquam in hendrerit urna.
+            </p>
+            <div className="flex gap-4">
+              <Link href="/authentication/sign-up">
+                <Button size="lg">Get Started</Button>
+              </Link>
+              <Button size="lg" variant="ghost">
+                <p className="underline">View more details</p>
+              </Button>
+            </div>
+          </div>
+          <div className=" h-[400px] flex items-center justify-end">
+            <Image
+              src="/heroimage.jpg"
+              width={422}
+              height={0}
+              alt="Hero"
+              className="top-0 right-0 "
+            />
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
