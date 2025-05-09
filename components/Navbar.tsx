bde71ba0342393cd68694eea88c39f3024bf73b7
Nuwan Thilakarathna <tinuwan@gmail.com>
2025-05-09 13:28:55 +0530

    chore: add @radix-ui/react-dropdown-menu dependency

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,8 +5,16 @@ import Link from "next/link";
 import { ArrowUpRight, CircleAlert, Menu, UserRound } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { Badge } from "@/components/ui/badge";
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import { usePathname } from "next/navigation";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Button } from "./ui/button";
 
 interface NavBarProps {
   variant?: "landing" | "app" | "visa";
@@ -21,14 +29,12 @@ const Navbar = ({
 
   activeItem,
 }: NavBarProps) => {
-  const [isVisible, setIsVisible] = useState(true);
-
   // get navigation
   const navigation = usePathname();
   console.log("navigation", navigation);
 
   // if navigation == "/visa-process" then set variant to visa
-  if (navigation === "/visa-process") {
+  if (navigation === "/visa-process" || navigation === "/visa-process/apply") {
     showVisa = true;
     variant = "visa";
   } else {
@@ -42,26 +48,10 @@ const Navbar = ({
 
   const user = session?.data?.user;
 
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 100) {
-        setIsVisible(false);
-      } else {
-        setIsVisible(true);
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
   return (
     <header className="static top-0 z-1000 w-full p-0 m-0 flex flex-col ">
       {/* upper bar */}
-      <nav
-        className={`sm:h-8 h-14 w-full bg-primary-500 flex items-center mx-0 p-0 m-auto transition-transform duration-300 ${
-          isVisible ? "translate-y-0" : "-translate-y-full"
-        }`}>
+      <nav className="sm:h-8 h-14 w-full bg-primary-500 flex items-center mx-0 p-0 m-auto transition-transform duration-300 ">
         <div className=" mx-[16px] sm:mx-[64px] md:mx-[120px]  h-6 w-full flex sm:gap-4 items-center justify-center flex-col sm:flex-row text-xs sm:text-sm font-bold text-white">
           <p>Visa processing approval system now changed! </p>
           <Link href="/visa-process" className="flex items-center gap-1">
@@ -76,124 +66,166 @@ const Navbar = ({
 
       {/* navbar */}
       <nav
-        className={` w-full bg-white flex items-center m-auto transition-all duration-300 ${
-          isVisible
-            ? "translate-y-0 sm:h-32 h-24"
-            : "sm:-translate-y-8  -translate-y-14 py-0 sm:h-20 h-16"
-        }`}>
+        className="sm:h-16 h-14 w-full bg-white flex items-center m-auto transition-all duration-300 ${
+          ">
         <div className=" mx-[16px] sm:mx-[64px] md:mx-[120px] h-full  w-full flex sm:gap-4 items-center justify-between  text-xs sm:text-sm font-bold">
           {/* left side */}
+
+          <div>
+            {/*plexlk logo */}
+            <Link href="/" className="flex items-center">
+              <div className="h-full sm:w-36 w-20">
+                <Image
+                  src="/logo.png"
+                  alt="plexlk logo"
+                  width={132}
+                  height={26.94}
+                  layout="responsive"
+                />
+              </div>
+              {showVisa && (
+                <Badge
+                  variant="secondary"
+                  className=" bg-primary-500 text-white mb-8 ">
+                  Visa
+                </Badge>
+              )}
+            </Link>
+          </div>
+
+          {/* middle  */}
           {user && (
-            <div>
-              {/*plexlk logo */}
-              <Link href="/" className="flex items-center">
-                <div className="h-full sm:w-36 w-20">
-                  <Image
-                    src="/logo.png"
-                    alt="plexlk logo"
-                    width={132}
-                    height={26.94}
-                    layout="responsive"
-                  />
-                </div>
-                {showVisa && (
-                  <Badge
-                    variant="secondary"
-                    className=" bg-primary-500 text-white mb-8 ">
-                    Visa
-                  </Badge>
-                )}
-              </Link>
+            <div className="hidden sm:flex items-center gap-4 md:gap-6/ font-normal sm:text-[16px] text-[14px] font-bold text-gray-500">
+              {variant !== "visa" && (
+                <nav className="hidden sm:flex  items-center sm:gap-4 md:gap-6/ font-normal sm:text-[16px] text-[14px] font-bold text-gray-500">
+                  <Link
+                    href="/explore"
+                    className={cn(
+                      "transition-colors hover:text-foreground/80",
+                      activeItem === "explore"
+                        ? "text-foreground font-medium"
+                        : "text-foreground/60"
+                    )}>
+                    Explore
+                  </Link>
+                  <Link
+                    href="/feed"
+                    className={cn(
+                      "transition-colors hover:text-foreground/80",
+                      activeItem === "feed"
+                        ? "text-foreground font-medium"
+                        : "text-foreground/60"
+                    )}>
+                    Feed
+                  </Link>
+                  <Link
+                    href="/leaderboard"
+                    className={cn(
+                      "transition-colors hover:text-foreground/80",
+                      activeItem === "leaderboard"
+                        ? "text-foreground font-medium"
+                        : "text-foreground/60"
+                    )}>
+                    Leaderboard
+                  </Link>
+                </nav>
+              )}
+
+              {variant === "visa" && (
+                <nav className="hidden sm:flex  items-center sm:gap-4 md:gap-6/ font-normal sm:text-[16px] text-[14px] font-bold text-gray-500">
+                  <Link
+                    href="/visa-process/apply"
+                    className={cn(
+                      "transition-colors hover:text-foreground/80",
+                      activeItem === "visa-process"
+                        ? "text-foreground font-medium"
+                        : "text-foreground/60"
+                    )}>
+                    Apply Now
+                  </Link>
+                  <Link
+                    href="/contact-us"
+                    className={cn(
+                      "transition-colors hover:text-foreground/80",
+                      activeItem === "visa-process"
+                        ? "text-foreground font-medium"
+                        : "text-foreground/60"
+                    )}>
+                    Contact Us
+                  </Link>
+                </nav>
+              )}
             </div>
           )}
 
-          {/* middle  */}
-          <div className="hidden sm:flex items-center gap-4 md:gap-6/ font-normal sm:text-[16px] text-[14px] font-bold text-gray-500">
-            {variant !== "visa" && (
-              <nav className="hidden sm:flex  items-center sm:gap-4 md:gap-6/ font-normal sm:text-[16px] text-[14px] font-bold text-gray-500">
-                <Link
-                  href="/explore"
-                  className={cn(
-                    "transition-colors hover:text-foreground/80",
-                    activeItem === "explore"
-                      ? "text-foreground font-medium"
-                      : "text-foreground/60"
-                  )}>
-                  Explore
-                </Link>
-                <Link
-                  href="/feed"
-                  className={cn(
-                    "transition-colors hover:text-foreground/80",
-                    activeItem === "feed"
-                      ? "text-foreground font-medium"
-                      : "text-foreground/60"
-                  )}>
-                  Feed
-                </Link>
-                <Link
-                  href="/leaderboard"
-                  className={cn(
-                    "transition-colors hover:text-foreground/80",
-                    activeItem === "leaderboard"
-                      ? "text-foreground font-medium"
-                      : "text-foreground/60"
-                  )}>
-                  Leaderboard
-                </Link>
-              </nav>
-            )}
-
-            {variant === "visa" && (
-              <nav className="hidden sm:flex  items-center sm:gap-4 md:gap-6/ font-normal sm:text-[16px] text-[14px] font-bold text-gray-500">
-                <Link
-                  href="/visa-process"
-                  className={cn(
-                    "transition-colors hover:text-foreground/80",
-                    activeItem === "visa-process"
-                      ? "text-foreground font-medium"
-                      : "text-foreground/60"
-                  )}>
-                  Apply Now
-                </Link>
-                <Link
-                  href="/visa-process"
-                  className={cn(
-                    "transition-colors hover:text-foreground/80",
-                    activeItem === "visa-process"
-                      ? "text-foreground font-medium"
-                      : "text-foreground/60"
-                  )}>
-                  Contact Us
-                </Link>
-              </nav>
-            )}
-          </div>
-
           {/* right side */}
-          <div className="flex items-center gap-2">
-            {variant !== "visa" && (
-              <div className="bg-yellow-50 p-2   w-auto flex items-center justify-center rounded-full text-amber-900 text-xs">
-                <p className="flex items-center gap-1">
-                  5 PLX <CircleAlert className="w-4 h-4"></CircleAlert>
-                </p>
-              </div>
-            )}
-            {variant !== "landing" && (
-              <div className="p-1 border-2  border-gray-400 sm:border-none sm:h-12 h-10 rounded-full flex gap-2 justify-between items-center">
-                <UserRound
-                  className="bg-gray-400 rounded-full w-auto h-full"
-                  color="white"></UserRound>
-
-                <div>
-                  <Menu className="w-auto h-full sm:hidden" color="black">
-                    {" "}
-                  </Menu>
+          {user && (
+            <div className="flex items-center gap-2">
+              {variant !== "visa" && (
+                <div className="bg-yellow-50 p-2   w-auto flex items-center justify-center rounded-full text-amber-900 text-xs">
+                  <p className="flex items-center gap-1">
+                    5 PLX <CircleAlert className="w-4 h-4"></CircleAlert>
+                  </p>
                 </div>
-              </div>
-            )}
-            {variant === "landing" && <div></div>}
-          </div>
+              )}
+              {variant !== "landing" && (
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <div className="p-1 border-2  border-gray-400 sm:border-none sm:h-12 h-10 rounded-full flex gap-2 justify-between items-center">
+                      <UserRound
+                        className="bg-gray-400 rounded-full w-auto h-full"
+                        color="white"></UserRound>
+                    </div>
+                  </DropdownMenuTrigger>
+
+                  <DropdownMenuContent
+                    className="w-40 flex flex-col items-center bg-white text-center"
+                    align="end">
+                    <div className="sm:hidden">
+                      <DropdownMenuItem>
+                        <Link href="/explore">Explore</Link>
+                      </DropdownMenuItem>
+                      <DropdownMenuItem>
+                        <Link href="/feed">Feed</Link>
+                      </DropdownMenuItem>
+                      <DropdownMenuItem>
+                        <Link href="/leaderboard">Leaderboard</Link>
+                      </DropdownMenuItem>
+                    </div>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>
+                      <Link href="/account">
+                        <span className="font-bold text-md">Account</span>
+                      </Link>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                      <Button
+                        onClick={() => signOut()}
+                        variant="ghost"
+                        className="w-full text-primary-500">
+                        <span className="font-bold text-md">Sign Out</span>
+                      </Button>
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              )}
+            </div>
+          )}
+
+          {!user && variant !== "landing" && (
+            <div className="hidden sm:flex items-center gap-2">
+              <Link href="/authentication/sign-in">
+                <Button variant="ghost" className="h-10 px-4">
+                  Sign In
+                </Button>
+              </Link>
+              <Link href="/authentication/sign-up">
+                <Button variant="outline" className="h-10 px-4 rounded-full">
+                  Get Started
+                </Button>
+              </Link>
+            </div>
+          )}
         </div>
       </nav>
     </header>
