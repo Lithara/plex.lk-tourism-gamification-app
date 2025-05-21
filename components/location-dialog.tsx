de77c52e11abaf0900f13b0d1171d96c4da608fa
Lithara <litharaperera2002@gmail.com>
2025-05-21 11:34:44 +0530

    location validation  slug changed to nsbm

--- components/location-dialog.tsx
+++ components/location-dialog.tsx
@@ -44,7 +44,7 @@ export function LocationDialog({
         if (isNearby(getGeoLocation, { latitude: lat, longitude: lng })) {
           setCanFlag(true);
         } else {
-          if (slug === "sigiriya") {
+          if (slug === "nsbm") {
             setCanFlag(true);
           } else {
             setCanFlag(false);
